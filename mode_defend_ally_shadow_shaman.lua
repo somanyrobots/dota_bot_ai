42d13f5949cff93e6780d5003110a8660aad1b32
somanyrobots <somanyrobots@gmail.com>
2016-12-15 17:00:40 -0800

    embyronic shadow shaman bot

--- mode_defend_ally_shadow_shaman.lua
+++ mode_defend_ally_shadow_shaman.lua
@@ -1,5 +1,4 @@
-
-require( GetScriptDirectory().."/mode_defend_ally_generic" )
+require( GetScriptDirectory().."/mode_defend_ally_generic" );
 
 ----------------------------------------------------------------------------------------------------
 
@@ -19,20 +18,20 @@ end
 
 function Think()
 
-	local npcBot = GetBot();
+	-- local npcBot = GetBot();
 
 	-- Do the standard Think
 	mode_generic_defend_ally.Think()
 
 	-- Check if we're already using an ability
 	if ( npcBot:IsUsingAbility() ) then return end;
-	
-	-- If we have a target and can cast LSA on them, do so
+
+	-- If we have a target and can cast Hex on them, do so
 	if ( npcBot:GetTarget() ~= nil ) then
-		abilityLSA = npcBot:GetAbilityByName( "lina_light_strike_array" );
-		if ( abilityLSA:IsFullyCastable() )
+		abilityHex = npcBot:GetAbilityByName( "shadow_shaman_voodoo" );
+		if ( abilityHex:IsFullyCastable() )
 		then
-			npcBot:Action_UseAbilityOnLocation( abilityLSA, npcBot:GetTarget():GetLocation() );
+			npcBot:Action_UseAbilityOnLocation( abilityHex, npcBot:GetTarget():GetLocation() );
 		end
 	end
 end
@@ -44,11 +43,11 @@ function GetDesire()
 	local npcBot = GetBot();
 	local fBonus = 0.0;
 
-	-- If we have a target and can cast Light Strike Array, our desire to help defend should be higher than normal
+	-- If we have a target and can cast Hex, our desire to help defend should be higher than normal
 	if ( npcBot:GetTarget() ~= nil )
 	then
-		abilityLSA = npcBot:GetAbilityByName( "lina_light_strike_array" );
-		if ( abilityLSA:IsFullyCastable() )
+		abilityHex = npcBot:GetAbilityByName( "shadow_shaman_voodoo" );
+		if ( abilityHex:IsFullyCastable() )
 		then
 			fBonus = 0.25;
 		end
