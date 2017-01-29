4de5f42daddf78d8dee536b4446a2771b7c5a47a
somanyrobots <somanyrobots@gmail.com>
2017-01-29 00:19:33 -0800

    implement basic pick-by-role system
    Right now it's very naive pick any-three-cores and any-two-supports

--- hero_selection.lua
+++ hero_selection.lua
@@ -44,14 +44,130 @@ allBotHeroes = {
 		'npc_dota_hero_zuus'
 };
 
+allHeroesByRole = {
+  core = {
+		'npc_dota_hero_axe',
+		'npc_dota_hero_bloodseeker',
+		'npc_dota_hero_bristleback',
+		'npc_dota_hero_chaos_knight',
+		'npc_dota_hero_death_prophet',
+		'npc_dota_hero_dragon_knight',
+		'npc_dota_hero_drow_ranger',
+		'npc_dota_hero_juggernaut',
+		'npc_dota_hero_kunkka',
+		'npc_dota_hero_luna',
+		'npc_dota_hero_necrolyte',
+		'npc_dota_hero_nevermore',
+		'npc_dota_hero_sand_king',
+		'npc_dota_hero_skeleton_king',
+		'npc_dota_hero_sniper',
+		'npc_dota_hero_sven',
+		'npc_dota_hero_tiny',
+		'npc_dota_hero_vengefulspirit',
+		'npc_dota_hero_windrunner',
+		'npc_dota_hero_zuus'
+  },
+  support = {
+		'npc_dota_hero_bane',
+		'npc_dota_hero_bounty_hunter',
+		'npc_dota_hero_crystal_maiden',
+		'npc_dota_hero_dazzle',
+		'npc_dota_hero_earthshaker',
+		'npc_dota_hero_jakiro',
+		'npc_dota_hero_lich',
+		'npc_dota_hero_lina',
+		'npc_dota_hero_lion',
+		'npc_dota_hero_omniknight',
+		'npc_dota_hero_oracle',
+		'npc_dota_hero_phantom_assassin',
+		'npc_dota_hero_pudge',
+		'npc_dota_hero_razor',
+		'npc_dota_hero_skywrath_mage',
+		'npc_dota_hero_tidehunter',
+		'npc_dota_hero_vengefulspirit',
+		'npc_dota_hero_warlock',
+		'npc_dota_hero_witch_doctor',
+  }
+};
+
+allHeroesWithRoles = {
+  npc_dota_hero_axe               = 'core',
+  npc_dota_hero_bane              = 'support',
+  npc_dota_hero_bloodseeker       = 'core',
+  npc_dota_hero_bounty_hunter     = 'support',
+  npc_dota_hero_bristleback       = 'core',
+  npc_dota_hero_chaos_knight      = 'core',
+  npc_dota_hero_crystal_maiden    = 'support',
+  npc_dota_hero_dazzle            = 'support',
+  npc_dota_hero_death_prophet     = 'core',
+  npc_dota_hero_dragon_knight     = 'core',
+  npc_dota_hero_drow_ranger       = 'core',
+  npc_dota_hero_earthshaker       = 'support',
+  npc_dota_hero_jakiro            = 'support',
+  npc_dota_hero_juggernaut        = 'core',
+  npc_dota_hero_kunkka            = 'core',
+  npc_dota_hero_lich              = 'support',
+  npc_dota_hero_lina              = 'support',
+  npc_dota_hero_lion              = 'support',
+  npc_dota_hero_luna              = 'core',
+  npc_dota_hero_necrolyte         = 'core',
+  npc_dota_hero_nevermore         = 'core',
+  npc_dota_hero_omniknight        = 'support',
+  npc_dota_hero_oracle            = 'support',
+  npc_dota_hero_phantom_assassin  = 'core',
+  npc_dota_hero_pudge             = 'core',
+  npc_dota_hero_razor             = 'core',
+  npc_dota_hero_sand_king         = 'support',
+  npc_dota_hero_skeleton_king     = 'core',
+  npc_dota_hero_skywrath_mage     = 'support',
+  npc_dota_hero_sniper            = 'core',
+  npc_dota_hero_sven              = 'core',
+  npc_dota_hero_tidehunter        = 'support',
+  npc_dota_hero_tiny              = 'core',
+  npc_dota_hero_vengefulspirit    = 'support',
+  npc_dota_hero_viper             = 'core',
+  npc_dota_hero_warlock           = 'support',
+  npc_dota_hero_windrunner        = 'core',
+  npc_dota_hero_witch_doctor      = 'support',
+  npc_dota_hero_zuus              = 'core'
+};
+
 picks = {};
 allSlots = {};
 
+-- TODO
+-- 4. implement various smart picking strategies
+function Think()
+  local radiantSlots = GetTeamPlayers(TEAM_RADIANT);
+  local direSlots = GetTeamPlayers(TEAM_DIRE);
+  for k,v in pairs(radiantSlots) do allSlots[v] = v end
+  for k,v in pairs(direSlots) do allSlots[v] = v end
+
+  if not ShouldPick() then
+    return;
+  end
+
+	if ( GetTeam() == TEAM_RADIANT ) then
+    slot = radiantSlots[RandomInt(1, #radiantSlots)];
+
+		if (IsPlayerBot(slot) and IsSlotEmpty(slot)) then
+			PickHero(slot);
+			return;
+		end
+	elseif ( GetTeam() == TEAM_DIRE ) then
+    slot = direSlots[RandomInt(1, #direSlots)];
+
+		if (IsPlayerBot(slot) and IsSlotEmpty(slot)) then
+			PickHero(slot);
+			return;
+		end
+	end
+end
+
 -- decides if it's a good time to pick
 -- if there are no humans in the match, all picks should happen instantly
---   (but in random order)
 -- if there are humans, picks should not start right away, and then should occur
---   in random order, at regular-but-jittery intervals
+--   at regular-but-jittery intervals
 function ShouldPick()
   local minimumPickTime = -70; -- no picks should happen earlier than this
   local timePerPick;
@@ -84,39 +200,6 @@ function ShouldPick()
   return true
 end
 
--- TODO
--- 1. determine which slots contain players - don't pick for those slots
--- 2. determine which slots are assigned to which teams - enables strategy
--- 3. add some jitter, so the bots pick at slightly more random times
--- 4. implement various smart picking strategies
--- 5. activate quick mode once all humans have picked
-function Think()
-  local radiantSlots = GetTeamPlayers(TEAM_RADIANT);
-  local direSlots = GetTeamPlayers(TEAM_DIRE);
-  for k,v in pairs(radiantSlots) do allSlots[v] = v end
-  for k,v in pairs(direSlots) do allSlots[v] = v end
-
-  if not ShouldPick() then
-    return;
-  end
-
-	if ( GetTeam() == TEAM_RADIANT ) then
-    slot = radiantSlots[RandomInt(1, #radiantSlots)];
-
-		if (IsPlayerBot(slot) and IsSlotEmpty(slot)) then
-			PickHero(slot);
-			return;
-		end
-	elseif ( GetTeam() == TEAM_DIRE ) then
-    slot = direSlots[RandomInt(1, #direSlots)];
-
-		if (IsPlayerBot(slot) and IsSlotEmpty(slot)) then
-			PickHero(slot);
-			return;
-		end
-	end
-end
-
 -- probably you're always passing allSlots to this
 function HaveAllHumansPicked(slots)
   for i,slot in pairs(slots) do
@@ -131,19 +214,39 @@ end
 function IsSlotEmpty(slot)
   local slotEmpty = true;
   for pickedSlot, hero in pairs(picks) do
-    -- print("pickedSlot is", pickedSlot);
-    -- print("hero is", hero);
 		if (pickedSlot == slot) then
 			slotEmpty = false;
 		end
   end
-  -- print("slotempty is ", slotEmpty);
   return slotEmpty;
 end
 
 function PickHero(slot)
-  local hero = GetRandomHero();
-  -- print("picking hero ", hero, " for slot ", slot);
+  local nCores = 0;
+  local nSupports = 0;
+  local slots = (GetTeam() == TEAM_RADIANT) and GetTeamPlayers(TEAM_RADIANT) or GetTeamPlayers(TEAM_DIRE);
+
+  for index,teamSlot in pairs(slots) do
+    local hName = GetSelectedHeroName(teamSlot);
+		if (hName ~= nil and hName ~= "") then
+      local role = allHeroesWithRoles[hName];
+      if role == 'core' then
+        nCores = nCores + 1
+      elseif role == 'support' then
+        nSupports = nSupports + 1
+      end
+    end
+  end
+
+  local hero;
+  if (nCores >= 3) then
+    hero = GetRandomHero('support');
+  elseif (nSupports >= 2) then
+    hero = GetRandomHero('core');
+  else
+    hero = GetRandomHero();
+  end
+
   SelectHero(slot, hero);
 end
 
@@ -162,22 +265,31 @@ end
 
 -- first, check the list of required heroes and pick from those
 -- then try the whole bot pool
-function GetRandomHero()
+function GetRandomHero(role)
 	local hero;
 	local picks = GetPicks(allSlots);
   local selectedHeroes = {};
+  local pickableHeroes;
+  role = role or nil;
   for slot, hero in pairs(picks) do
     selectedHeroes[hero] = true;
   end
 
+  if (role == nil) then
+    pickableHeroes = allBotHeroes;
+  elseif (role == 'core') then
+    pickableHeroes = allHeroesByRole['core'];
+  elseif (role == 'support') then
+    pickableHeroes = allHeroesByRole['support'];
+  end
+
 	hero = requiredHeroes[RandomInt(1, #requiredHeroes)];
 	if (hero == nil) then
-		hero = allBotHeroes[RandomInt(1, #allBotHeroes)];
+		hero = pickableHeroes[RandomInt(1, #pickableHeroes)];
 	end
 
 	while ( selectedHeroes[hero] == true ) do
-		-- print("repicking because " .. hero .. " was taken.")
-		hero = allBotHeroes[RandomInt(1, #allBotHeroes)];
+		hero = pickableHeroes[RandomInt(1, #pickableHeroes)];
 	end
 
 	return hero;
